fcb1e29a311ae7fa9910c434fc2a71531fa4614b
GlekKk <113007138+GlekKk@users.noreply.github.com>
2026-02-19 12:51:34 +0200

    Add files via upload

---
+++ src/dataset.py
@@ -0,0 +1,47 @@
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+IMG_SIZE = 224
+BATCH_SIZE = 64
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+
+def get_data_loaders(data_dir):
+
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.Resize((IMG_SIZE, IMG_SIZE)),
+            transforms.RandomHorizontalFlip(p=0.5), 
+            transforms.RandomVerticalFlip(p=0.5),   
+            transforms.RandomRotation(30),          
+            transforms.ColorJitter(brightness=0.1, contrast=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize(MEAN, STD)]),
+        'val': transforms.Compose([
+            transforms.Resize((IMG_SIZE, IMG_SIZE)),
+            transforms.ToTensor(),
+            transforms.Normalize(MEAN, STD)]),
+        'test': transforms.Compose([
+            transforms.Resize((IMG_SIZE, IMG_SIZE)),
+            transforms.ToTensor(),
+            transforms.Normalize(MEAN, STD)])
+    }
+
+    image_datasets = {
+        x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+        for x in ['train', 'val', 'test']
+    }
+
+    dataloaders = {
+        x: DataLoader(image_datasets[x], 
+                      batch_size = BATCH_SIZE, 
+                      shuffle = (x == 'train'), 
+                      num_workers = 2)
+        for x in ['train', 'val', 'test']
+    }
+    
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val', 'test']}
+    class_names = image_datasets['train'].classes
+    
+    return dataloaders, dataset_sizes, class_names
